06627e3adbf4bbfb288742f0d4a09563463c9f22
felipe bernal martinez <el_biker_felipebm@hotmail.com>
2024-08-04 14:13:02 +0200

    change colors

--- LadyPorcia Web/components/landing/Cards.vue
+++ LadyPorcia Web/components/landing/Cards.vue
@@ -30,7 +30,7 @@ const getImageAbsolutePath = (imageName: string): string => {
             <a :href="href">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{{title}}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-black">{{description}}</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{description}}</p>
             <LandingLink :href="href" size="md" block :styleName="styleName">Más información</LandingLink>
         </div>
     </div>
