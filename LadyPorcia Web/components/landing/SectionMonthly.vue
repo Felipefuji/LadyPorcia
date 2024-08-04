2d45068ac614826737ba6302844281998f28c41b
felipe bernal martinez <el_biker_felipebm@hotmail.com>
2024-08-04 14:06:10 +0200

    add all workshops

---
+++ LadyPorcia Web/components/landing/SectionMonthly.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  align: {
+    default: "left",
+  },
+});
+</script>
+
+<template>
+  <div :class="['mt-16', align === 'left' && 'text-left']">
+    <h1 class="text-1xl lg:text-2xl font-bold lg:tracking-tight">
+      <slot name="month">Month</slot>
+    </h1>
+  </div>
+</template>
